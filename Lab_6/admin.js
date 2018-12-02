d0abe17f87ee5ee484c7a648d08b82fee3aadbe5
romanlevonovyy <32957243+romanlevonovyy@users.noreply.github.com>
2018-12-02 19:04:54 +0200

    Lab_6

---
+++ Lab_6/admin.js
@@ -0,0 +1,9 @@
+function processing() {
+    var text = document.getElementsByTagName("input")[0];
+    var val = text.value;
+    alert(val);
+//    var new_element = document.createElement('div');
+//    new_element.className = "someNewClass";
+//    new_element.innerHTML = val;
+//    test.append(new_element);
+}
